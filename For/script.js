1d29546efa516800e57d4e5617ca2278621415c2
RadiantF-pondok <71006921+RadiantF-pondok@users.noreply.github.com>
2020-09-12 06:48:33 +0700

    update lagii
    aaaa

--- For/script.js
+++ For/script.js
@@ -1,11 +1,7 @@
-var listDesa = ('argodadi,sedayu', 'argomulyo,sedayu', 'argorejo,sedayu', 'argosari', 'bangunjiwo,kasihan', 'banguntapan,banguntapan', 'bantul,bantul', 'baturetno,banguntapan', 'bawuran,pleret', 'canden,jetis', 'caturharjo,pandak', 'cetan srigandi');
-var data = 0;
-
-
-document.write('<ol>')
-for (i = 0; i < listDesa.length; i++) {
-    document.write("</li>")
-    document.write("<li>", listDesa.length[i], "</li>")
-    document.write('</li>')
+function luasPersegiPanjang(p, l) {
+    return p * l;
 }
-document.write("</ol>")
+// luasPersegiPanjang(5, 9);
+document.write('Panjang: 5<br>Lebar: 9<br>')
+document.write('Luas persegi panjang: ')
+document.write(luasPersegiPanjang(5, 9));
